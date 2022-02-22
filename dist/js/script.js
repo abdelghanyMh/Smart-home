43da597fe34e783e23c34151a94e475cdc5a2cea
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-02-22 09:08:53 +0100

    add:  fadIn & fadeOut animations OnScroll
    Using the Intersection Observer API

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,9 +9,42 @@ function hideNavBar(event) {
     toggleBtn.checked = false;
 }
 
-
-
 // add events listners for the navbar links
 links.forEach(element => {
     element.addEventListener('click', hideNavBar);
 });
+
+
+/** the Intersection Observer API code start **/
+
+const animationItems = document.querySelectorAll(".animate");
+const inView = "animate-in-view";
+const outView = "animate-out-view";
+
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+
+                entry.target.classList.remove(outView);
+                entry.target.classList.add(inView);
+            }
+            else {
+                entry.target.classList.remove(inView);
+                entry.target.classList.add(outView);
+
+            }
+        });
+    },
+    {
+        threshold: .20,
+        rootMargin: '-100px'
+    }
+);
+animationItems.forEach(item => {
+    observer.observe(item)
+})
+
+/** the Intersection Observer API code end **/
